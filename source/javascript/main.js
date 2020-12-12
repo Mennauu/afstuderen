86b26795df91c8a9b9de8dc2f410ecd659d72253
Mennauu <mennodevries1@msn.com>
2020-12-12 12:57:46 +0100

    Initial commit

---
+++ source/javascript/main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/*------------------------------------*\
+ * JS Main entry file
+ \*------------------------------------*/
+import './config'
+import '@utilities/detect-touch'
+import '@utilities/detect-keyboard-focus'
+import '@utilities/polyfills'
+import '@components/image'
+
+import moduleInit from '@/utilities/module-init'
+// import Example from '@components/example' // Sync
+// moduleInit.sync('[js-hook-module-example]', Example) // Sync
+
+moduleInit.async('[js-hook-module-example]', () => import('@components/example')) // Async
